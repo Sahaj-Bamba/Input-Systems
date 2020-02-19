eba24550ac6a0a8354785278370a1b14570ee7fa
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-02-20 00:42:58 +0530

    Really Completed Receive part

---
+++ Assets/Scripts/Receive.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Receive : MonoBehaviour
+{
+    public bool isActive;
+    private string action;
+    private GameObject Manager;
+    public TextMeshProUGUI msg;
+
+    void Start()
+    {
+        Manager = GameObject.Find("GameManager");
+        Debug.Log("Receive Start");
+        Activate();
+    }
+
+    public void Activate(){
+        isActive = true;
+    }
+    
+    void FixedUpdate()
+    {
+        if (isActive)
+        {
+            action = Manager.GetComponent<UDPHandller>().action;
+			Manager.GetComponent<UDPHandller>().action = null;
+            if (action == "" || action == null)
+            {
+                return;
+            }
+            else {
+                Add(action);
+            }
+        }
+    }
+
+    private void Add(string str){
+        msg.text += str + "/n";
+    }
+
+}
+
--- Assets/Scripts/UDPReceiver.cs
+++ Assets/Scripts/UDPReceiver.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UDPReceiver : MonoBehaviour
@@ -14,8 +10,6 @@ public class UDPReceiver : MonoBehaviour
 
     private Thread receiveThread;
 	private UdpClient client;
-	// private int port;
-	// private string ip;
     private GameObject Manager;
     private string action;
 	
@@ -48,10 +42,11 @@ public class UDPReceiver : MonoBehaviour
 		client = new UdpClient (int.Parse(port.text));
 		while (true)
 		{
+			IPEndPoint anyIP;
 			if(ip.text == "Any"){
-				IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse(ip.text), port);
+				anyIP = new IPEndPoint(IPAddress.Parse(ip.text), int.Parse(port.text));
 			}else{
-				IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, port);
+				anyIP = new IPEndPoint(IPAddress.Any, int.Parse(port.text));
 			}
 			byte[] data = client.Receive(ref anyIP);
 			string text = Encoding.UTF8.GetString(data);
