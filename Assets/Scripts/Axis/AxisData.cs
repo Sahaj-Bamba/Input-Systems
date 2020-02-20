8b105dc9a7bf3693b52b462599bd8f7c2dc4fee3
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-02-20 19:28:13 +0530

    Axis Sender done

---
+++ Assets/Scripts/Axis/AxisData.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class AxisData
+{
+    public AxisData(float a,float b){
+        horizontal = a;
+        vertical = b;
+    }
+    
+    public float horizontal;
+    public float vertical;  
+
+    override public string ToString(){
+        return " Horizontal : " + horizontal + " /n Vertical : " + vertical;
+    } 
+}
---
+++ Assets/Scripts/Axis/AxisSender.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
+using System.Text;
+
+public class AxisSender : MonoBehaviour
+{
+    private IPEndPoint anyIP;
+    private UdpClient client;
+    private int port=9875;
+    private TextMeshProUGUI msg;
+
+    void Start()
+    {
+        anyIP = new IPEndPoint(IPAddress.Any, port);
+        client = new UdpClient();
+    }
+
+    public void send(){
+
+        AxisData axisData = new AxisData(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        msg.text = axisData.ToString();
+        string st = JsonUtility.ToJson(axisData);
+        byte[] data = Encoding.UTF8.GetBytes(st); 
+        client.Send(data, data.Length, anyIP);
+
+    }
+
+    private void FixedUpdate() {
+        send();
+    }
+
+}
